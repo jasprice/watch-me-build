8eed4540e4f4aac3ade1ceb7cf3830bd23abbbd8
Jason Price <jason_price@yahoo.com>
2024-11-08 13:46:54 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,24 +9,25 @@ const Index = () => {
           <span className="inline-block px-4 py-2 bg-[#2d2d4a] rounded-full text-sm mb-6">
             Launching Soon!
           </span>
+          <br></br><br></br>
           <h1 className="text-5xl font-bold mb-4">Join our Waitlist!</h1>
           <p className="text-lg mb-6">
             Discover how creators bring their digital products to life! From websites and apps to designs 
             and videos, we showcase the unique creative processes behind the scenes. <br></br><br></br>
             Join us to explore how different creators build and shape their digital world.
           </p>
+        </div>
+        <EmailCaptureForm />
+      </div>
+      <div className="absolute inset-0 bg-[url('/network-bg.png')] opacity-10 z-0"></div>
+    </div>
           <p className="text-sm">
             For those who would like to be featured, drop us an{' '}
             <a href="mailto:pricedigitalventures@gmail.com" className="underline">
               email
             </a>{' '}
             so we can get you scheduled.
-          </p>
-        </div>
-        <EmailCaptureForm />
-      </div>
-      <div className="absolute inset-0 bg-[url('/network-bg.png')] opacity-10 z-0"></div>
-    </div>
+          </p>    
   );
 };
 
